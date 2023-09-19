e6dae84c7316b97f9cbb68288d29816380bf1f17
gulsute <134259376+gulsute@users.noreply.github.com>
2023-09-19 17:01:43 +0300

    polindrome done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("-------q1---------");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+        System.out.println("-------q2---------");
+
+    }
+
+    public static boolean isPalindrome (int num) {
+      num = Math.abs(num);
+      char [] digits = String.valueOf(num).toCharArray();
+      String reversed = "";
+      for (int i=digits.length-1; i>=0; i--) {
+          reversed += digits[i];
+      }
+      return reversed.equals(String.valueOf(num));
     }
 }
