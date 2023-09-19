4d26cce2bee10e6aafc127fbe479ca7e8fb19856
gulsute <134259376+gulsute@users.noreply.github.com>
2023-09-19 17:19:37 +0300

    tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ public class Main {
         System.out.println(isPalindrome(707));
         System.out.println(isPalindrome(11212));
         System.out.println("-------q2---------");
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+        System.out.println(isPerfectNumber(28));
+        System.out.println("-------q3---------");
+        System.out.println(numberToWords(123));
+        System.out.println(numberToWords(1010));
+        System.out.println(numberToWords(-12));
 
     }
 
@@ -18,4 +26,62 @@ public class Main {
       }
       return reversed.equals(String.valueOf(num));
     }
+
+    public static boolean isPerfectNumber (int num) {
+        if (num<0) {
+            return false;
+        }
+
+        int total = 0;
+        for(int i =1; i<=num/2; i++) {
+            if(num % i == 0) {
+                total += i;
+            }
+        }
+        return num == total;
+    }
+
+    public static String numberToWords (int num) {
+        if (num < 0) {
+            return "Invalid value";
+        }
+        char [] digits = String.valueOf(num).toCharArray();
+        String text = "";
+        for (char digit:digits) {
+            switch (digit) {
+                case '0':
+                    text += "Zero ";
+                    break;
+                case '1':
+                    text += "One ";
+                    break;
+                case '2':
+                    text += "Two ";
+                    break;
+                case '3':
+                    text += "Three ";
+                    break;
+                case '4':
+                    text += "Four ";
+                    break;
+                case '5':
+                    text += "Five ";
+                    break;
+                case '6':
+                    text += "Six ";
+                    break;
+                case '7':
+                    text += "Seven ";
+                    break;
+                case '8':
+                    text += "Eight ";
+                    break;
+                case '9':
+                    text += "Nine ";
+                    break;
+            }
+        }
+        return text.trim();
+    }
+
 }
